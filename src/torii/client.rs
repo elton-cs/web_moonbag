50c39adeabddea26b393d4e7c62c6e1c8544329b
elton-cs <govada.elton@gmail.com>
2025-06-25 16:45:53 -0500

    chore: simplify create client

--- src/torii/client.rs
+++ src/torii/client.rs
@@ -5,17 +5,10 @@ use torii_client::Client;
 use torii_client::error::Error;
 
 pub async fn create_client(config: ToriiConfig) -> Result<Client, Error> {
-    let world_felt = Felt::from_hex_unchecked(&config.world_address);
-
-    match Client::new(config.url, world_felt).await {
-        Ok(client) => {
-            let metadata = client.metadata().await?;
-
-            info!(
-                "Connected to Torii world at address: {}",
-                metadata.world_address
-            );
-            Ok(client)
+    match Client::new(config.url, Felt::from_hex_unchecked(&config.world_address)).await {
+        Ok(c) => {
+            info!("Created Torii client.");
+            Ok(c)
         }
         Err(e) => {
             error!("Failed to create Torii client: {}", e);
