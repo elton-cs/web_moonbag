e1739585d256228ac43737bc1b45f5c076936359
elton-cs <govada.elton@gmail.com>
2025-06-30 19:33:11 -0500

    restart: clean build

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-// Support configuring Bevy lints within code.
-#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
-// Disable console on Windows for non-dev builds.
-#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
-
-mod asset_tracking;
-mod audio;
-mod background;
-mod demo;
-#[cfg(feature = "dev")]
-mod dev_tools;
-mod menus;
-mod screens;
-mod theme;
-mod torii;
-
 use bevy::{asset::AssetMetaCheck, prelude::*};
 
 fn main() -> AppExit {
@@ -24,13 +8,9 @@ pub struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
-        // Add Bevy plugins.
         app.add_plugins(
             DefaultPlugins
                 .set(AssetPlugin {
-                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
-                    // This causes errors and even panics on web build on itch.
-                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
                     meta_check: AssetMetaCheck::Never,
                     ..default()
                 })
@@ -44,63 +24,11 @@ impl Plugin for AppPlugin {
                     ..default()
                 }),
         );
+        app.add_plugins(());
 
-        // Add other plugins.
-        app.add_plugins((
-            // asset_tracking::plugin,
-            // audio::plugin,
-            // demo::plugin,
-            // #[cfg(feature = "dev")]
-            // dev_tools::plugin,
-            // menus::plugin,
-            // screens::plugin,
-            // theme::plugin,
-            background::plugin,
-            torii::plugin,
-        ));
-
-        // Order new `AppSystems` variants by adding them here:
-        app.configure_sets(
-            Update,
-            (
-                AppSystems::TickTimers,
-                AppSystems::RecordInput,
-                AppSystems::Update,
-            )
-                .chain(),
-        );
-
-        // Set up the `Pause` state.
-        app.init_state::<Pause>();
-        app.configure_sets(Update, PausableSystems.run_if(in_state(Pause(false))));
-
-        // Spawn the main camera.
         app.add_systems(Startup, spawn_camera);
     }
 }
-
-/// High-level groupings of systems for the app in the `Update` schedule.
-/// When adding a new variant, make sure to order it in the `configure_sets`
-/// call above.
-#[derive(SystemSet, Debug, Clone, Copy, Eq, PartialEq, Hash, PartialOrd, Ord)]
-enum AppSystems {
-    /// Tick timers.
-    TickTimers,
-    /// Record player input.
-    RecordInput,
-    /// Do everything else (consider splitting this into further variants).
-    Update,
-}
-
-/// Whether or not the game is paused.
-#[derive(States, Copy, Clone, Eq, PartialEq, Hash, Debug, Default)]
-#[states(scoped_entities)]
-struct Pause(pub bool);
-
-/// A system set for systems that shouldn't run while the game is paused.
-#[derive(SystemSet, Copy, Clone, Eq, PartialEq, Hash, Debug)]
-struct PausableSystems;
-
 fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Name::new("Camera"),
